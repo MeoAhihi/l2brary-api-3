525695181969ab32be95607387947d9b5ee92dac
ViPhong <viphongly2804@gmail.com>
2025-09-18 14:39:58 +0700

    feat(schedule): Add schedule validation schema using Mongoose for managing course schedules. Updated package.json and pnpm-lock.yaml to include mongoose and @nestjs/mongoose dependencies.

---
+++ src/modules/ld/course/schemas/schedule.schema.ts
@@ -0,0 +1,21 @@
+// schedule.validation.ts
+import mongoose from "mongoose";
+import { Weekday } from "../types/schedule.types";
+
+const { Schema } = mongoose;
+
+const scheduleDetailSchema = new Schema({
+  daysOfWeek: [
+    {
+      type: String,
+      enum: Weekday,
+    },
+  ],
+  dayOfMonth: { type: Number, min: 1, max: 31 },
+  dates: [{ type: Date }],
+});
+
+export const ScheduleValidator = mongoose.model(
+  "ScheduleValidation",
+  scheduleDetailSchema
+);
